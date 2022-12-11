9591b72817b0026359983a0c6e939cc1cdcdfbd5
森崇 <kanetugu2015@gmail.com>
2022-12-11 19:06:35 +0900

    add error handling

--- main/src/main.rs
+++ main/src/main.rs
@@ -6,8 +6,20 @@ fn main() {
         println!("Usage: {} <delta_msec> <max_delay_msec>", args[0]);
         std::process::exit(1);
     }
-    let delta_msec: u64 = args[1].parse::<u64>().expect("ERROR invalid delta_msec");
-    let max_delay_msec: u64 = args[2].parse::<u64>().expect("ERROR invalid max_delay_msec");
+    let delta_msec: u64 = match args[1].parse::<u64>() {
+        Ok(n) => n,
+        Err(e) => {
+            println!("ERROR delta_msec {}", e);
+            std::process::exit(1);
+        },
+    };
+    let max_delay_msec: u64 = match args[2].parse::<u64>() {
+        Ok(n) => n,
+        Err(e) => {
+            println!("ERROR max_delay_msec: {}", e);
+            std::process::exit(1);
+        },
+    };
 
     println!("delta_msec = {}", delta_msec);
     println!("max_delay_msec = {}", max_delay_msec);
