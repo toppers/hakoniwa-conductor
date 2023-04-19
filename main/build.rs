f464441e6494e196c744c6b32e36529ba6fbdeeb
s-hosoai <shintaro.hosoai@gmail.com>
2023-04-19 23:18:09 +0900

    rename hakoniwa-spec directory

--- main/build.rs
+++ main/build.rs
@@ -2,6 +2,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rustc-link-search=/usr/lib");
     println!("cargo:rustc-link-search=/usr/local/lib/hakoniwa");
-    tonic_build::compile_protos("hakoniwa-core-spec/hakoniwa_core.proto")?;
+    tonic_build::compile_protos("spec/hakoniwa_core.proto")?;
     Ok(())
 }
