a09e47d3d9404bb4c1a978e8029e121382194caa
森崇 <kanetugu2015@gmail.com>
2023-06-25 16:15:28 +0900

    add registration

--- main/src/client/mod.rs
+++ main/src/client/mod.rs
@@ -1,6 +1,7 @@
 use tonic::{ transport::Server, Request, Response, Status};
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
+use tonic::transport::{Endpoint, Uri};
 
 pub mod hakoniwa {
     tonic::include_proto!("hakoniwa");
@@ -18,13 +19,28 @@ use hakoniwa::{
     CreatePduChannelRequest, CreatePduChannelReply,
     SubscribePduChannelRequest, SubscribePduChannelReply
 };
-
-pub async fn start_service(ip_port: &String) -> Result<(), Box<dyn std::error::Error>>
+pub async fn start_service(ip: &String, port: &String) -> Result<(), Box<dyn std::error::Error>> 
 {
-    //TODO
-    //TODO create channel
-    //TODO register asset
-    //TODO monitor event
-        //TODO wait start, reset, ...etc..
+    let uri = format!("http://{}:{}", ip, port).parse::<Uri>()?;
+    let endpoint = Endpoint::from(uri);
+    let channel = endpoint.connect().await?;
+
+
+    // Create a client using the channel
+    let mut client = CoreServiceClient::new(channel);
+
+    // Create an AssetInfo message
+    let asset_info = AssetInfo {
+        name: "SampleAsset".to_string(),
+    };
+
+    // Send the register request
+    let request = tonic::Request::new(asset_info);
+    let response = client.register(request).await?;
+
+    // Process the response
+    let reply = response.get_ref();
+    println!("Register response: {:?}", reply);
+
     Ok(())
 }
--- main/src/main_client.rs
+++ main/src/main_client.rs
@@ -13,8 +13,8 @@ pub mod client;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        println!("Usage: {} <delta_msec> <max_delay_msec> <ipaddr>:<port>", args[0]);
+    if args.len() != 5 {
+        println!("Usage: {} <delta_msec> <max_delay_msec> <ipaddr> <port>", args[0]);
         std::process::exit(1);
     }
     let delta_msec: i64 = match args[1].parse::<i64>() {
@@ -31,15 +31,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             std::process::exit(1);
         }
     };
-    let v: Vec<&str> = args[3].split(':').collect();
-    let ipaddr: String = String::from(v[0]);
+    let ip: &String = &args[3];
+    let port: &String = &args[4];
 
     println!("delta_msec = {}", delta_msec);
     println!("max_delay_msec = {}", max_delay_msec);
     let delta_usec: i64 = delta_msec * 1000;
     let max_delay_usec: i64 = max_delay_msec * 1000;
-    hako::api::master_init(max_delay_usec, delta_usec);
-
+    //hako::api::master_init(max_delay_usec, delta_usec);
+    if let Err(e) = client::start_service(ip, port).await {
+        eprintln!("Error: {}", e);
+    }
     let s = chan_signal::notify(&[Signal::INT, Signal::TERM]);
     std::thread::spawn(move || {
         loop {
