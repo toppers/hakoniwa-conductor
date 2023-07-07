ad14e9d4ca1f25db3cf98c85b237493abc104fe2
森崇 <kanetugu2015@gmail.com>
2023-07-08 08:33:17 +0900

    add mqtt

--- main/src/client/mod.rs
+++ main/src/client/mod.rs
@@ -1,6 +1,11 @@
-//use tonic::{ transport::Server, Request, Response, Status};
-//use tokio::sync::mpsc;
-//use tokio_stream::wrappers::ReceiverStream;
+use chan;
+use chan_signal;
+use chan_signal::Signal;
+use chan::chan_select;
+use std::net::UdpSocket;
+use paho_mqtt as mqtt;
+use crate::hako;
+
 use tonic::transport::{Endpoint, Uri};
 use crate::{loader::{
     ConductorConfig, 
@@ -12,7 +17,6 @@ use crate::{loader::{
 pub mod hakoniwa {
     tonic::include_proto!("hakoniwa");
 }
-use crate::hako;
 
 use hakoniwa::{
     core_service_client:: { CoreServiceClient },
@@ -30,22 +34,17 @@ use hakoniwa::{
 };
 pub async fn start_service(conductor_config: ConductorConfig, robot_config_path: &String) -> Result<(), Box<dyn std::error::Error>> 
 {
+    //TODO
+    //hako::api::master_init(max_delay_usec, delta_usec);
+
     let uri = format!("http://{}:{}", conductor_config.core_ipaddr.clone(), conductor_config.core_portno.clone()).parse::<Uri>()?;
     let endpoint = Endpoint::from(uri);
     let channel = endpoint.connect().await?;
-
 
     // Create a client using the channel
     let mut client: CoreServiceClient<tonic::transport::Channel> = CoreServiceClient::new(channel);
 
     // Create an AssetInfo message
-    //TODO
-    //0. server向けには、自分のアセット名で、登録する
-    //1. 自分のアセット名は、外部定義ファイルから取得する。
-    //2. 全SUBSCRチャネルは、外部定義ファイルから取得する。
-    //3. 全CREATEチャネルは、外部定義ファイルから取得する。
-    //4. 全CREATEチャネルを登録する(publishチャネル)
-    //4. 全SUBSCRチャネルを登録する(subscribeチャネル)
     let asset_info = AssetInfo {
         name: conductor_config.asset_name.clone(),
     };
@@ -72,16 +71,57 @@ pub async fn start_service(conductor_config: ConductorConfig, robot_config_path:
             std::process::exit(1);
         }
     }
-
-
+    //CREATE UDP SOCKET
+    let socket: Option<UdpSocket> = Some(hako::method::udp::create_publisher_udp_socket(&conductor_config.udp_sender_ip_port));
+    hako::method::udp::activate_server(&conductor_config.udp_server_ip_port);
+    //MQTT SERVER
+    let mut cli: Option<mqtt::Client> = None;
+    if conductor_config.mqtt_portno > 0 {
+        hako::method::mqtt::set_mqtt_url(conductor_config.core_ipaddr.clone(), conductor_config.mqtt_portno);
+        cli = hako::method::mqtt::create_mqtt_publisher();
+    }
+    //EXEC SIMULATION
+    let delta_msec: u32 = conductor_config.delta_msec as u32;
+    let s = chan_signal::notify(&[Signal::INT, Signal::TERM]);
+    std::thread::spawn(move || {
+        loop {
+            let do_something = chan::after_ms(delta_msec as u32);
+            chan_select! {
+                s.recv() -> signal => {
+                    println!("signal={:?}", signal);
+                    std::process::exit(0);
+                },
+                do_something.recv() => {
+                    if hako::api::master_execute() {
+                        match socket {
+                            Some(ref _n) => {
+                                hako::method::udp::send_all_subscriber(socket.as_ref().unwrap());
+                            },
+                            None => ()
+                        }
+                        //println!("is_enabled={}", hako::method::mqtt::is_enabled() );
+                        if hako::method::mqtt::is_enabled() {
+                            //println!("start mqtt send1");
+                            match cli {
+                                Some(ref _n) => {
+                                    //println!("start mqtt send2");
+                                    hako::method::mqtt::publish_mqtt_topics(_n);
+                                },
+                                None => ()
+                            }
+                        }
+                    }
+                }
+            }
+        }    
+    });
+    //TODO START CLIENT SERVICE...
     Ok(())
 }
 
 async fn initialize_readers(client: &mut CoreServiceClient<tonic::transport::Channel>, conductor_config: &ConductorConfig, robot_config: &RobotConfig) -> Result<(), Box<dyn std::error::Error>> 
 {
     //TODO api::create_pdu_lchannel
-    //TODO rpc::subscribe_pdu_channel
-
     for robot in &robot_config.robots {
         for reader in &robot.rpc_pdu_readers {
             let request = SubscribePduChannelRequest {
--- main/src/main_client.rs
+++ main/src/main_client.rs
@@ -1,15 +1,7 @@
-#[macro_use]
-extern crate chan;
-extern crate chan_signal;
-use chan_signal::Signal;
 use std::env;
-//use std::net::UdpSocket;
-//use paho_mqtt as mqtt;
-
-//internal modules
-pub mod hako;
 pub mod client;
 pub mod loader;
+pub mod hako;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,14 +12,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     let conductor_config_path: &String = &args[1];
     let robot_config_path: &String = &args[2];
-    let mut delta_usec: i64 = 1000 * 10;
-    //let mut max_delay_usec = 1000 * 100;
     match loader::load_conductor_config(conductor_config_path) {
         Ok(conductor_config) => {
             loader::show_conductor_config(&conductor_config);
-            delta_usec = conductor_config.delta_msec * 1000;
-            //max_delay_usec = conductor_config.max_delay_msec * 1000;
-            //hako::api::master_init(max_delay_usec, delta_usec);
             if let Err(e) = client::start_service(conductor_config, &robot_config_path).await {
                 eprintln!("Error: client::strat_service() {}", e);
                 std::process::exit(1);
@@ -39,24 +26,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let s = chan_signal::notify(&[Signal::INT, Signal::TERM]);
-    std::thread::spawn(move || {
-        let delta_msec = delta_usec as u32 / 1000;
-        loop {
-            let do_something = chan::after_ms(delta_msec as u32);
-            chan_select! {
-                s.recv() -> signal => {
-                    println!("signal={:?}", signal);
-                    std::process::exit(0);
-                },
-                do_something.recv() => {
-
-                }
-
-            }
-        }
-    });
-
-    //future.await?;    
     Ok(())
 }
