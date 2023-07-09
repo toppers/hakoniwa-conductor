bd5741320d0ffbc88b1efca7fde46451136f01a8
Takashi Mori <kanetugu2015@gmail.com>
2023-07-09 12:52:00 +0900

    add callback

--- main/src/client/sim_executor/mod.rs
+++ main/src/client/sim_executor/mod.rs
@@ -1,12 +1,70 @@
 
 use crate::client::rpc_client;
 use crate::client::rpc_client::hakoniwa::core_service_client::CoreServiceClient;
+use crate::client::rpc_client::hakoniwa::AssetNotificationEvent;
 use crate::hako;
 
+
+async fn server_event_handling(client: &mut CoreServiceClient<tonic::transport::Channel>, asset_name: &String) -> Result<bool, Box<dyn std::error::Error>>
+{
+    let event: AssetNotificationEvent = rpc_client::get_simevent();
+    match event {
+        AssetNotificationEvent::Start => {
+            let ret = hako::api::simevent_start();
+            rpc_client::asset_notification_feedback(client, asset_name, event, ret).await?;
+        }
+        AssetNotificationEvent::Stop => {
+            let ret = hako::api::simevent_stop();
+            rpc_client::asset_notification_feedback(client, asset_name, event, ret).await?;
+        }
+        AssetNotificationEvent::Reset => {
+            let ret = hako::api::simevent_reset();
+            rpc_client::asset_notification_feedback(client, asset_name, event, ret).await?;
+        }
+        AssetNotificationEvent::None => {
+            //nothing to do
+        }
+        AssetNotificationEvent::Heartbeat => {
+            //nothing to do
+        }
+        AssetNotificationEvent::Error => {
+            //nothing to do
+        }
+    }
+    Ok(true)
+}
+
+fn asset_do_callback(asset_name: &String)-> Result<bool, Box<dyn std::error::Error>>
+{
+    let event: hako::api::SimulationAssetEventType = hako::api::asset_get_event(asset_name.clone());
+    match event {
+        hako::api::SimulationAssetEventType::Start => {
+            hako::api::asset_start_feedback(asset_name.clone(), true);
+        }
+        hako::api::SimulationAssetEventType::Stop => {
+            hako::api::asset_stop_feedback(asset_name.clone(), true);
+        }
+        hako::api::SimulationAssetEventType::Reset => {
+            hako::api::asset_reset_feedback(asset_name.clone(), true);
+        }
+        hako::api::SimulationAssetEventType::None => {
+            //nothing to do
+        }
+        hako::api::SimulationAssetEventType::Invalid => {
+            //nothing to do
+        }
+        hako::api::SimulationAssetEventType::Error => {
+            //nothing to do
+        }
+    }
+    Ok(true)
+}
 pub async fn execute(client: &mut CoreServiceClient<tonic::transport::Channel>, asset_name: &String) -> Result<bool, Box<dyn std::error::Error>>
 {
-    let is_sim_mode = hako::api::asset_is_simulation_mode();
+    server_event_handling(client, asset_name).await?;
+    asset_do_callback(&asset_name)?;
 
+    let is_sim_mode = hako::api::asset_is_simulation_mode();
     let is_read_pdu_done = is_sim_mode;
     let is_write_pdu_done = is_sim_mode;
     let asset_time = hako::api::asset_get_worldtime();
@@ -37,3 +95,4 @@ pub async fn execute(client: &mut CoreServiceClient<tonic::transport::Channel>, 
     hako::api::asset_notify_simtime(asset_name.clone(), status.master_time);
     Ok(hako::api::master_execute())
 }
+
