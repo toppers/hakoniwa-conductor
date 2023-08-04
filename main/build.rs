7371f738595425aa838d693a005249ef349066c9
Takashi Mori <kanetugu2015@gmail.com>
2023-08-04 14:25:37 +0900

    updated for m2mac

--- main/build.rs
+++ main/build.rs
@@ -1,7 +1,11 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rustc-link-search=/usr/lib");
-    println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+    if cfg!(target_arch = "x86_64") {
+        println!("cargo:rustc-link-search=/usr/lib/x86_64-linux-gnu");
+    } else if cfg!(target_arch = "aarch64") {
+        println!("cargo:rustc-link-search=/usr/lib/aarch64-linux-gnu");
+    }
     println!("cargo:rustc-link-search=/usr/local/lib/hakoniwa");
     tonic_build::compile_protos("spec/hakoniwa_core.proto")?;
     Ok(())
